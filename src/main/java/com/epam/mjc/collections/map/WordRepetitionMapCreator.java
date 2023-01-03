0b01444a7a3affa0fb7c35c8256fe9e188dc6415
Dmitry Golub <dmgolub@mail.ru>
2023-01-03 20:52:45 +0400

    Task completed.

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
+
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if (sentence.isEmpty()) {
+            return Map.of();
+        }
+        String[] words = sentence.toLowerCase().replaceAll("[^a-zA-Z ]", "").split(" ");
+        Map<String, Integer> wordsToCount = new HashMap<>();
+        for (String word : words) {
+            wordsToCount.computeIfPresent(word, (k, v) -> v + 1);
+            wordsToCount.putIfAbsent(word, 1);
+        }
+        return wordsToCount;
     }
 }
